81fe6dc5e7508ae09e5e01ca34f7b112b707f1ea
Raí Joia <megac154@gmail.com>
2024-05-12 01:35:30 -0300

    feat: nova logica para escolher 5 candidatos

--- src/candidatura/ProcessoSeletivo.java
+++ src/candidatura/ProcessoSeletivo.java
@@ -1,12 +1,37 @@
 package candidatura;
 
+import java.util.concurrent.ThreadLocalRandom;
+
 public class ProcessoSeletivo {
 	public static void main(String[] args) {
 		System.out.println("Processo Seletivo");
-		analisarCandidato(2000.0);
-		analisarCandidato(2200.0);
-		analisarCandidato(1500.0);
+
+		selecaoCandidatos();
 	}
+	
+	static void selecaoCandidatos() {
+		String[] candidatos = {"RAÍ", "Julia", "CRISTIANE", "EDUARDO", "MONIQUE", "VANDA", "ALEMAO", "BIANCA", "CLEIDE", "CHARLIE"};
+		int candidatosSelecionados = 0;
+		int candidatoAtual = 0;
+		double salarioBase = 2000.0;
+		
+		while(candidatosSelecionados < 5 && candidatoAtual < candidatos.length) {
+			String candidato = candidatos[candidatoAtual];
+			double salarioPretendido = valorPretendido();
+			
+			System.out.println("O candidato " + candidato + " solicitou esse valor de salário " + salarioPretendido);
+			if(salarioBase >= salarioPretendido) {
+				candidatosSelecionados++;
+				System.out.println("O candidato " + candidato + " Foi selecionado para a vaga");
+			}
+			candidatoAtual++;
+		}
+	}
+	
+	static double valorPretendido() {
+		return ThreadLocalRandom.current().nextDouble(1800, 2200);
+	}
+	
 	static void analisarCandidato(double salarioPretendido) {
 		double salarioBase = 2000.0;
 		if(salarioBase > salarioPretendido) {
